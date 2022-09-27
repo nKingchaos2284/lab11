0f8e079b1b3db30bd3e0a9b4454542809e5ffc8b
J M Cabrera Elias <jmcabrerae@gmail.com>
2022-09-27 09:26:01 -0700

    update things

--- js/app.js
+++ js/app.js
@@ -1,121 +1,15 @@
 'use strict';
-
-/*NOT FINAL SUBMISSION, STILL WORKING. NOTES ARE IN CODE
-*/
-
-
-// global varriables
-
-let goatContainer = document.getElementById('section');
-let resultButton = document.getElementById('section + div');
-let voteCount = 25;
-let goatArray = []; 
-let image1 = document.getElementById('section img:first-child');
-let image2 = document.getElementById('section img:nth-child(2)');
-
-// let clicks = 0;
-// let maxClicksAllowed = 9;
-
-// functional logic
-
-new.Goat('bunny-goat', 'png');
-yada yada yada
-
-function Goat(name, fileExtension = 'jpeg') {
-  this.name = name;
-  this.img = `img/${dragon.jpg}.${sweep.jpg}`;
-  this.views = 0;
-  this.clicks = 0;
-  Goat.allGoatsArray.push(this);
-}
-
-new Goat {}
-
-Goat.allGoatsArray = [];
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * Goat.allGoatsArray.length);
-}
-
-function renderGoats() {
-  // call the getRandomNumber
-  let goat1 = getRandomNumber();
-  let goat2 = getRandomNumber();
-
-  while (goat1 === goat2) {
-    goat2 = getRandomNumber();
-  }
-  image1.src = Goat.allGoatsArray[goat1].src;
-  image2.src = Goat.allGoatsArray[goat2].src;
-  image1.alt = Goat.allGoatsArray[goat1].name;
-  image2.alt = Goat.allGoatsArray[goat2].name;
-  Goat.allGoatsArray[goat1].views++;
-  Goat.allGoatsArray[goat2].views++;
-}
-
-function handleGoatClick(event) {
-  if (event.target === goatContainer) {
-    alert('Please click on an image');
-  }
-  clicks++;
-  let clickGoat = event.target.alt;
-  for (let i = 0; i < Goat.allGoatsArray.length; i++) {
-    if (clickGoat === Goat.allGoatsArray[i].name) {
-      Goat.allGoatsArray[i].clicks++;
-      break;
-    }
-  }
-  if (clicks === maxClicksAllowed) {
-    goatContainer.removeEventListener('click', handleGoatClick);
-    // give the button an event lister and styles so the user
-    // knows its an active button:
-    resultButton.addEventListener('click', renderResults);
-    resultButton.className = 'clicks-allowed';
-    goatContainer.className = 'no-voting';
-  } else {
-    renderGoats();
-  }
-}
-
-function renderResults() {
-  let ul = document.querySelector('ul');
-  for (let i = 0; i < Goat.allGoatsArray.length; i++) {
-    let li = document.createElement('li')
-    li.textContent = `${Goat.allGoatsArray[i].name} had ${Goat.allGoatsArray[i].views} view and was clicked ${Goat.allGoatsArray[i].clicks} times.`;
-    ul.appendChild(li);
-  }
-}
-
-// executable code
-
-new Goat('Cruising Goat', './images/cruisin-goat.jpg');
-new Goat('Float Your Goat', './images/float-your-goat.jpg');
-new Goat('Goat Out of Hand', './images/goat-out-of-hand.jpg');
-new Goat('Kissing Goat', './images/kissing-goat.jpg');
-new Goat('Sassy Goat', './images/sassy-goat.jpg');
-new Goat('Smiling Goat', './images/smiling-goat.jpg');
-new Goat('Sweater Goat', './images/sweater-goat.jpg');
-
-renderGoats();
-
-goatContainer.addEventListener('click', handleGoatClick);
-
-
-
-*/////////NOTES
-
-'use strict';
 /******************************************************************
  *  Global Variables
  ******************************************************************/
-let fantaseaContainer; //html elements will go here
+let thingContainer; //html elements will go here
 let resultButton;// will show the voting results
 let image1; //  1st image element
 let image2; // 2nd image element
 let image3;// 3rd image product
-let allProductsArray;// an array of product objects
+let allProductsArray;// array of product objects
 let click = 0; // # of user clicks
-let maxClicksPermitted = 25; // the maximum amount that a user can  click.
+let maxClicksPermitted = 25; // the max user can  click.
 
 
 
@@ -155,23 +49,16 @@ console.log(productArray);
 
 function Product(productName,imagePath){
   this.productName = productName;
-  this.imagePath= imagePath;
+  this.imagePath = imagePath;
   this.views=0;
   this.clicks=0;
 }
 
 
-/************************************************************************
-  * LOGIC
-  ***********************************************************************
-  /
-/**
- * Draw 3 random products on the page
- */
 
 
 function render(){
-  console.log('rendering()');
+  console.log('makeItSoNumber2()');
 
   // finding previous values
 
@@ -191,6 +78,7 @@ function render(){
   let product1 = getRandomProductIndex();
   let product2 = getRandomProductIndex();
   let product3 = getRandomProductIndex();
+  
   ///how to make sure they're not the same product
   while (product1===product2 || product1===product3){
     product1=getRandomProductIndex();
@@ -233,7 +121,7 @@ function renderResult(){
 /********************************************************************************
  * Control Logic
  */
-/** initialize global variables, setting up event handlers and initial render performance */
+
 function initialize (){
   console.log('in initialize()');
   // initial references to html
